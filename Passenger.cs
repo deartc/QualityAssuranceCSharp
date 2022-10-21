33e30106c921bf5cc7ee6855cd9599812c594126
Teresa Hawkins <74428141+deartc@users.noreply.github.com>
2022-10-21 14:08:32 -0400

    Delete Passenger.cs

--- Passenger.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PracticeAssignment1
-{
-    public class Employee
-    {
-        public string Name { get; set; }
-        public DateTime Birthday { get; set; }
-        public decimal Salary { get; set; }
-    }
-}
