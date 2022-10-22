3bc5f3f47fa0852c357b2e0b4f5f327a5a2425a9
Teresa Hawkins <74428141+deartc@users.noreply.github.com>
2022-10-22 16:09:28 -0400

    commit car class

---
+++ CodeLouisvilleUnitTestProject/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+
+
+namespace
+Vehicle
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int GasTankCapacity { get; set; }
+        public double GasLevel { get; set; }
+        public double MilesPerGallon { get; set; }
+        public double Odometer { get; set; } = 0;
+        public double MilesRemaining { get; set; }
+
+
+
+
+
+        public Car(string make, string model, int GasTankCapacity, double milesPerGallon)
+        {
+            Make = make;
+            Model = model;
+            GasTankCapacity = gasTankCapacity;
+
+            GasLevel = gasLevel;
+            MilesPerGallon = milesPerGallon;
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              
+
+
