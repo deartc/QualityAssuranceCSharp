2e517330b8a66c02ccaa872265e7700eea92e8f5
Teresa Hawkins <74428141+deartc@users.noreply.github.com>
2022-10-25 23:44:07 -0400

    first

--- CodeLouisvilleUnitTestProject/Car.cs
+++ CodeLouisvilleUnitTestProject/Car.cs
@@ -1,39 +1,62 @@
-﻿
+﻿using CodeLouisvilleUnitTestProject;
+using System.Reflection;
+
 namespace using CodeLouisvilleUnitTestProject;
 
 public class Car : Vehicle
-{
-    public string Make { get; set; }
-    public string Model { get; set; }
-    public int Year { get; set; }
+{  
+    {
+        #region Public Properties
+        public string Make { get; }
+        public string Model { get; }
+        public double MilesPerGallon { get; set; }
+        public double GasTankCapacity { get; init; }
+        public int NumberOfTires { get; init; }
+        public int Year { get; set; }
     }
 }
+#region Private Fields
+private double _gasRemaining;
+private double _mileage;
+private bool _hasFlatTire;
+#endregion
 
-namespace QualityAssuranceCSharp.CodeLouisvilleUnitTestProject
+
+
+
+public Car (int numberOfTires, double gasTankCapacity, string make, string model, double milesPerGallon, int year)
+{
+    NumberOfTires = numberOfTires;
+    GasTankCapacity = gasTankCapacity;
+    Make = make;
+    Model = model;
+    MilesPerGallon = milesPerGallon;
+    Year = year
+}
+
+
 {
     class Car
     {
         static void Main(string[] args)
         {
-            Tire tire = new Tire { Name = "Goodyear", Model = "Fx100", };
 
-            Console.WriteLine("Created a wheel {0}, model {1}", Tire.Name, Tire.Model,);
 
-            Vehicle car = new Vehicle { Make = "Honda", Model = "Civic" Year = “2020” };
+        Car = new car { Make = "Honda", Model = "Civic" Year = “2020” };
+
+            Car = new car  { Make = "Toyota", Model = "Prius" Year = “2020” };
+
+            Vehicle car = new Vehicle { Make = "Sabaru", Model = "Civic" Year = “2020” };
             //vehicle.Make = "Honda";
             //vehicle.Model = "Civic";
             //vehicle.Year = “2020”
 
-            for (int i = 0; i < 4; i++)
+            
             {
-                car.AddTire(Tire);
-            }
 
-            Console.WriteLine("Created a vehicle {0}, model {1}", car.Make, car.Model);
 
-            Console.WriteLine(car.ToString());
 
-            Console.ReadKey();
+
         }
     }
 }
