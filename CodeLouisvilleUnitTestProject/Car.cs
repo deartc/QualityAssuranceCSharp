c689f6ae2f93c090065f473981778f33c87b7296
Teresa Hawkins <74428141+deartc@users.noreply.github.com>
2022-10-28 19:46:30 -0400

    commit

--- CodeLouisvilleUnitTestProject/Car.cs
+++ CodeLouisvilleUnitTestProject/Car.cs
@@ -1,4 +1,5 @@
-﻿using Vehicle;
+﻿using QualityAssuranceCSharp.CodeLouisvilleUnitTestProject;
+using Vehicle;
 using static MyApplication.CarDetail;
 using static System.Net.Mime.MediaTypeNames;
 using car = Vehicle.car;
@@ -92,10 +93,70 @@ public class car
 
     
         }
+}
+}
+
+///passenger console
+
+//passenger 
+	
+
+	using Car
+	using System.Text.Json;
+	
+
+	Console.WriteLine("This is passenger menu. Please choose from the following menu options:");
+Console.WriteLine("1: Add an passenger");
+Console.WriteLine("2: View all current passengers");
+Console.WriteLine("3: Remove an passenger by name");
+Console.WriteLine("4: Exit");
+
+
+List<Passenger> passengers = new List<Passenger>();
+
+
+var options = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true
+};
+
+
+var fileText = File.ReadAllText("passengers.json");
+var deserialized = JsonSerializer.Deserialize<List<Passenger>>(fileText, options);
+passengers.AddRange(deserialized);
+
+
+string userInput = Console.ReadLine();
+
+
+while (userInput != "4")
+{
+    if (userInput == "1")
+    {
+        passengers = Menu.AddPassenger(passengers);
     }
+    else if (userInput == "2")
+    {
+        Menu.DisplayPassengers(passengers);
+    }
+    else if (userInput == "3")
+    {
+        Console.WriteLine("Enter the name of the passenger to remove");
+        var name = Console.ReadLine();
+        Menu.RemovePassenger(passengers, name);
+    }
+    else if (userInput == "4") //Exit
+    {
+
+
+    }
+    Console.WriteLine("What next?");
+    userInput = Console.ReadLine();
 }
 
 
+var json = JsonSerializer.Serialize(passengers);
+File.WriteAllText("passengers.json", json);
 
 
 Console.WriteLine("Please add or remove  passengers by  the following menu options:");
@@ -107,12 +168,6 @@ Console.WriteLine("Enter the name of the passenger to remove");
 
 Console.WriteLine("4: Exit");
 
-Console.WriteLine("Testing for gas and flat tires!");
-
-Console.WriteLine("Cannot drive, out of gas.”.\r\n");
-
-
-Console.WriteLine("Cannot drive due to flat tire");
 
 
 
---
+++ Tire.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace QualityAssuranceCSharp
+{
+    internal class Tire
+    {
+        Console.WriteLine("Testing for gas and flat tires!");
+
+Console.WriteLine("Cannot drive, out of gas.”.\r\n");
+
+
+Console.WriteLine("Cannot drive due to flat tire");
+    }
+}
