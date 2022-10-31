f6db027d2a1ceca9349f9c26ebb3c02259d7eec9
Teresa Hawkins <74428141+deartc@users.noreply.github.com>
2022-10-31 10:40:40 -0400

    commit car tests

---
+++ SemiTruck/SemiTruckWheelsTest/CarTest3.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+     
+namespace CodeLouisvilleUnitTestProjectTests
+{
+    public class CarTests
+    {
+        [Fact]
+        public void CreateNewCarTest()
+        {
+            //arrange & act
+            var car = new Car()
+
+            //assert
+            Assert.IsFalse(test_car.GasTankLevel == 0);
+
+            Assert("Cannot drive, out of gas.”");
+
+        }
+    }
+
+
+
+    //Car Test, out of gas
